3b280427ff775c6f3062f107b9123073cf96f474
cena tabriziyan <cena.tabriziyan@gmail.com>
2020-06-05 21:56:33 +0430

    create login and signup xml

---
+++ app/src/main/java/ir/cena/logginmvvm/ui/auth/Loginactivity.kt
@@ -0,0 +1,12 @@
+package ir.cena.logginmvvm.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.cena.logginmvvm.R
+
+class Loginactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
